0e20985ec1a1f3ed5d7242ba3dec8e632841af65
time2fishman <adam_h_martinez@hotmail.com>
2023-12-06 20:08:24 -0700

    add validation check and error message

--- src/App.js
+++ src/App.js
@@ -4,18 +4,32 @@ import Selection from './components/Selection';
 function App() {
   const [numOfPlayers, setNumOfPlayers] = useState()
   const [numOfGames, setNumOfGames] = useState()
+  const [numOfCourts, setNumOfCourts] = useState()
+  const [errorMessage, setErrorMessage] = useState('')
 
   const handlePlayerSelect = (event) => {
     setNumOfPlayers(event.target.value)
+    setErrorMessage('')
+  }
+
+  const handleCourtSelect = (event) => {
+    setNumOfCourts(event.target.value)
+    setErrorMessage('')
   }
 
   const handleGamesSelect = (event) => {
     setNumOfGames(event.target.value)
+    setErrorMessage('')
   }
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    console.log(`you submited ${numOfPlayers} players and ${numOfGames} games`)
+    // validation
+    if (5 * numOfCourts < numOfPlayers) {
+      setErrorMessage('This configuration leaves too many people sitting. Only 1 bye per court is allowed.')
+    }
+
+    console.log(`you submited ${numOfPlayers} players, ${numOfCourts} of courts, and ${numOfGames} games`)
   }
 
   return (
@@ -24,6 +38,8 @@ function App() {
         handleSubmit={handleSubmit}
         handlePlayerSelect={handlePlayerSelect}
         handleGamesSelect={handleGamesSelect}
+        handleCourtSelect={handleCourtSelect}
+        errorMessage={errorMessage}
       />
     </div>
   );
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,25 +1,40 @@
 import 'tachyons'
 
-const Selection = ({ handlePlayerSelect, handleGamesSelect, handleSubmit }) => {
+const Selection = ({ handlePlayerSelect, handleGamesSelect, handleCourtSelect, handleSubmit, errorMessage }) => {
+
   return (
     <>
-      <h1 className='tc'>Pickleball Round Robin Mixer</h1>
+      <h1 className='tc mt5'>Pickleball Round Robin Mixer</h1>
       <form className='tc' onSubmit={handleSubmit}>
         <label className='f4' htmlFor='numberOfPlayers'>
           Select number of players:
           <select onChange={handlePlayerSelect} className='ml2 pt1 pr2 pb1 pl2' name='numberOfPlayers' id='numberOfPlayers' required>
             <option></option>
-            <option value={4}>4</option>
-            <option value={5}>5</option>
-            <option value={6}>6</option>
-            <option value={7}>7</option>
             <option value={8}>8</option>
             <option value={9}>9</option>
+            <option value={10}>10</option>
+            <option value={11}>11</option>
+            <option value={12}>12</option>
+            <option value={13}>13</option>
+            <option value={14}>14</option>
+            <option value={15}>15</option>
+            <option value={16}>16</option>
+          </select>
+        </label>
+        <br></br>
+        <label className='f4' htmlFor='numberOfCourts'>
+          Select number of courts:
+          <select onChange={handleCourtSelect} className='ml2 mt2 pt1 pr2 pb1 pl2' name='numberOfCourts' id='numberOfCourts' required>
+            <option></option>
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
           </select>
         </label>
         <br></br>
         <label className='f4' htmlFor='numberOfGames'>
-          Select number of Games:
+          Select number of games:
           <select onChange={handleGamesSelect} className='ml2 mt2 pt1 pr2 pb1 pl2' name='numberOfGames' id='numberOfGames' required>
             <option></option>
             <option value={1}>1</option>
@@ -33,7 +48,8 @@ const Selection = ({ handlePlayerSelect, handleGamesSelect, handleSubmit }) => {
             <option value={9}>9</option>
           </select>
           <br></br>
-          <input className='mt2' type='submit' value='Randomize' />
+          <input className='mt3 pa1' type='submit' value='Submit' />
+          {errorMessage && (<p className="red">{errorMessage}</p>)}
         </label>
       </form>
     </>
